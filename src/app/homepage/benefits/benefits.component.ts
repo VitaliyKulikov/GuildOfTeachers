d2ef41f6f5074856d21f87ac524ba2000286ce51
norka2233 <norka2233@gmail.com>
2019-04-06 20:23:24 +0300

    new commit

---
+++ src/app/homepage/benefits/benefits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-benefits',
+  templateUrl: './benefits.component.html',
+  styleUrls: ['./benefits.component.sass']
+})
+export class BenefitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
