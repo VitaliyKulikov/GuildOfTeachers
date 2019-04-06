2ba1e75941661a5c48fddfe3fef8fbcf73e8719c
Sofia <sofiahaivoronsk@gmail.com>
2019-04-06 14:26:24 +0300

    homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import {HomePageModule } from './homepage/homepage.module';
+import { RegistrationComponent } from './registration/registration.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
