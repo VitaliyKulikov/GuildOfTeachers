d10b6cd58a4d7b2b323d766a367a5efd8ba396cc
norka2233 <norka2233@gmail.com>
2019-04-06 16:49:48 +0300

    first commits

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HomepageComponent } from './homepage.component';
 import { HomePageRoutingModule } from './homepage-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { HeroBannerComponent } from './hero-banner/hero-banner.component';
 @NgModule({
     declarations: [
-        HomepageComponent
+        HomepageComponent,
+        HeaderComponent,
+        HeroBannerComponent
     ],
     imports: [
         BrowserModule,
