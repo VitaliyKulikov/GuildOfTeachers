5e20d359853735b3a47e1fded056f6eca3e635ed
norka2233 <norka2233@gmail.com>
2019-04-06 22:21:43 +0300

    fix another merge conflict

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { HeroBannerComponent } from './hero-banner/hero-banner.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BenefitsComponent } from './benefits/benefits.component';
+import { UpdatesComponent } from './updates/updates.component';
 @NgModule({
     declarations: [
         HomepageComponent,
         HeaderComponent,
         HeroBannerComponent,
         AboutUsComponent,
-        BenefitsComponent
+        BenefitsComponent,
+        UpdatesComponent
     ],
     imports: [
         BrowserModule,
