3828a371b285dc796b4133297d1bdac0b5c8702f
yuriimchg <yurgan4@gmail.com>
2019-04-06 21:50:21 +0300

    sofas changas

---
+++ src/app/our-teacher/filter-side/filter-side.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilterSideComponent } from './filter-side.component';
+
+describe('FilterSideComponent', () => {
+  let component: FilterSideComponent;
+  let fixture: ComponentFixture<FilterSideComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FilterSideComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FilterSideComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
