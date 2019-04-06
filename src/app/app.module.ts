e2102dfc6e359ce1ddef82d005d96997fb965852
Sofia <sofiahaivoronsk@gmail.com>
2019-04-06 23:15:11 +0300

    registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,28 @@ import { RegistrationModule } from './registration/registration.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../app/modules/material/material.module';
 import { OurTeacherModule } from './our-teacher/our-teacher.module';
+import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
+
+export function jwtOptionsFactory() {
+  return {
+    tokenGetter: () => {
+      return localStorage.getItem('access_token');
+    },
+    whitelistedDomains: ['localhost:8100', 'localhost:3000']
+  };
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule, OurTeacherModule,
-    AppRoutingModule, HomePageModule, RegistrationModule, BrowserAnimationsModule, MaterialModule
+    AppRoutingModule, HomePageModule, RegistrationModule, BrowserAnimationsModule, MaterialModule, JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
