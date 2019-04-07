4a2a0a0e555caebea35d0603d6c57f07edc1eb58
yuriimchg <yurgan4@gmail.com>
2019-04-07 10:01:44 +0300

    fix merge conflict again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { RegistrationModule } from './registration/registration.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../app/modules/material/material.module';
 import { OurTeacherModule } from './our-teacher/our-teacher.module';
-<<<<<<< HEAD
-=======
 import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 export function jwtOptionsFactory() {
@@ -21,23 +19,18 @@ export function jwtOptionsFactory() {
     whitelistedDomains: ['localhost:8100', 'localhost:3000']
   };
 }
->>>>>>> 4fa6acdf761cd4bf3c2a423de73f94ea0ae01d7f
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule, OurTeacherModule,
-<<<<<<< HEAD
-    AppRoutingModule, HomePageModule, RegistrationModule, BrowserAnimationsModule, MaterialModule
-=======
     AppRoutingModule, HomePageModule, RegistrationModule, BrowserAnimationsModule, MaterialModule, JwtModule.forRoot({
       jwtOptionsProvider: {
         provide: JWT_OPTIONS,
         useFactory: jwtOptionsFactory
       }
     })
->>>>>>> 4fa6acdf761cd4bf3c2a423de73f94ea0ae01d7f
   ],
   providers: [],
   bootstrap: [AppComponent]
